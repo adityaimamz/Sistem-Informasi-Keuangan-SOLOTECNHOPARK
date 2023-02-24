256104c09807f58aa90ac9c282b96d50bebc7e62
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-02-24 11:12:35 +0700

    add login page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,40 @@
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="/css/style.css" />
+     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+     <title>Ludiflex | Login</title>
+
+ </head>
+ <body>
+    <div class="box">
+     <div class="container login-panel">
+         <div class="top">
+             <span class="text-login">Silahkan Login Terlebih dahulu</span>
+             <header class="header-login">Login</header>
+         </div>
+
+         <div class="input-field">
+             <input type="text" class="input" placeholder="Username" id="">
+             <i class='bx bx-user logo' ></i>
+         </div>
+
+         <div class="input-field">
+             <input type="Password" class="input" placeholder="Password" id="">
+             <i class='bx bx-lock-alt logo'></i>
+         </div>
+
+         <div class="input-field">
+            <button class="button2">
+                Hover me
+            </button>
+         </div>
+     </div>
+ </div>
+ </body>
+ </html>
