4d7aba848ae2ec4408d539c800b371e72ef6dd5f
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-03-06 11:06:31 +0700

    add master sumberdana

---
+++ PHP native/keuangan/admin/dana.php
@@ -0,0 +1,186 @@
+<?php include 'header.php'; ?>
+
+<div class="content-wrapper">
+
+  <section class="content-header">
+    <h1>
+      dana
+      <small>Data dana</small>
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active">Dashboard</li>
+    </ol>
+  </section>
+
+  <section class="content">
+    <div class="row">
+      <section class="col-lg-12">
+        <div class="box box-info">
+
+          <div class="box-header">
+            <h3 class="box-title">Data Sumber Dana</h3>
+            <div class="btn-group pull-right">            
+
+              <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#exampleModal">
+                <i class="fa fa-plus"></i> &nbsp Tambah Sumber Dana
+              </button>
+            </div><hr>
+            <?php 
+                if(isset($_GET['alert'])){
+                  if($_GET['alert']=='gagal'){
+                    ?>
+                    <div class="alert alert-warning alert-dismissible">
+                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                      <h4><i class="icon fa fa-warning"></i> Peringatan !</h4>
+                      Ekstensi Tidak Diperbolehkan
+                    </div>								
+                    <?php
+                  }elseif($_GET['alert']=="berhasil"){
+                    ?>
+                    <div class="alert alert-success alert-dismissible">
+                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                      <h4><i class="icon fa fa-check"></i> Success</h4>
+                      Berhasil Disimpan
+                    </div> 								
+                    <?php
+                  }elseif($_GET['alert']=="berhasilupdate"){
+                    ?>
+                    <div class="alert alert-success alert-dismissible">
+                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                      <h4><i class="icon fa fa-check"></i> Success</h4>
+                      Berhasil Update
+                    </div> 								
+                    <?php
+                  }
+                }
+                ?>
+          </div>
+          <div class="box-body">
+
+            <!-- Modal -->
+            <form action="dana_proses.php" method="post" enctype="multipart/form-data">
+              <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h4 class="modal-title" id="exampleModalLabel">Tambah Sumber Dana</h4>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+
+                      <div class="form-group">
+                        <label>JENIS SUMBER DANA</label>
+                        <input type="text" name="jenis" required="required" class="form-control" placeholder="Masukkan Nama ..">
+                      </div>
+
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                      <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+
+
+            <div class="table-responsive">
+              <table class="table table-bordered table-striped" id="table-datatable">
+                <thead>
+                  <tr>
+                    <th>NO</th>
+                    <th>JENIS dana BAYAR</th>
+                    <th>OPSI</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <?php 
+                  include '../koneksi.php';
+                  $no=1;
+                  $data = mysqli_query($koneksi,"SELECT * FROM master_sumberdana order by Id_sumberdana desc");
+                  while($d = mysqli_fetch_array($data)){
+                    ?>
+                    <tr>
+                      <td class="text-center"><?php echo $no++; ?></td>
+                      <td><?php echo $d['Jenis']; ?></td>
+                      <td>    
+                        <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#edit_dana_<?php echo $d['Id_sumberdana'] ?>">
+                          <i class="fa fa-cog"></i>
+                        </button>
+
+                        <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#hapus_dana_<?php echo $d['Id_sumberdana'] ?>">
+                          <i class="fa fa-trash"></i>
+                        </button>
+
+                        <form action="dana_update.php" method="post" enctype="multipart/form-data">
+                          <div class="modal fade" id="edit_dana_<?php echo $d['Id_sumberdana'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h4 class="modal-title" id="exampleModalLabel">Edit dana</h4>
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                  </button>
+                                </div>
+                                <div class="modal-body">
+
+                                  <div class="form-group" style="width:100%;margin-bottom:20px">
+                                    <label>JENIS SUMBER DANA</label>
+                                    <input type="hidden" name="id" value="<?php echo $d['Id_sumberdana'] ?>">
+                                    <input type="text" style="width:100%" name="jenis" required="required" class="form-control" placeholder="Masukkan Nama .." value="<?php echo $d['Jenis'] ?>">
+                                  </div>
+
+                                </div>
+                                <div class="modal-footer">
+                                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                                  <button type="submit" class="btn btn-primary">Simpan</button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </form>
+
+                        <!-- modal hapus -->
+                        <div class="modal fade" id="hapus_dana_<?php echo $d['Id_sumberdana'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                          <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h4 class="modal-title" id="exampleModalLabel">Peringatan!</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                              </div>
+                              <div class="modal-body">
+
+                                <p>Yakin ingin menghapus data ini ?</p>
+
+                              </div>
+                              <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                                <a href="dana_hapus.php?id=<?php echo $d['Id_sumberdana'] ?>" class="btn btn-primary">Hapus</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+                      </td>
+                    </tr>
+                    <?php 
+                  }
+                  ?>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </div>
+  </section>
+
+</div>
+<?php include 'footer.php'; ?>
