6629fb550d9a852ed0039e7beb45dcf97c2bc25d
ADITYA IMAM ZUHDI <100418056+adityaimamz@users.noreply.github.com>
2023-03-13 11:22:25 +0700

    Update coba.php

--- PHP native/keuangan/admin/coba.php
+++ PHP native/keuangan/admin/coba.php
@@ -79,7 +79,6 @@
 				datasets: [{
 					label: '',
 					data: [
-<<<<<<< HEAD
 						<?php
       $divisi = mysqli_query($koneksi, "SELECT DISTINCT Id_divisi FROM master_pengeluaran ORDER BY Id_divisi ASC");
       while($d = mysqli_fetch_array($divisi)){
@@ -150,52 +149,6 @@
        }
      }
       ?>
-=======
-					<?php 
-					$jumlah_administrasi = mysqli_query($koneksi,"SELECT sum(Jumlah) from master_pengeluaran where Id_divisi='12'");
-					echo number_format(mysqli_fetch_array($jumlah_administrasi)[0]);
-					?>, 
-					<?php 
-					$jumlah_akuntansi = mysqli_query($koneksi,"SELECT sum(Jumlah) from master_pengeluaran where Id_divisi='4'");
-					echo number_format(mysqli_fetch_array($jumlah_akuntansi)[000000]);
-					?>, 
-					<?php 
-					$jumlah_anggaran = mysqli_query($koneksi,"SELECT sum(Jumlah) from master_pengeluaran where Id_divisi='3'");
-					echo number_format(mysqli_fetch_array($jumlah_anggaran)[000000]);
-					?>, 
-					<?php 
-					$jumlah_diklat = mysqli_query($koneksi,"SELECT sum(Jumlah) from master_pengeluaran where Id_divisi='6'");
-					echo number_format(mysqli_fetch_array($jumlah_diklat)[000000]);
-					?>,
-					<?php 
-					$jumlah_kh = mysqli_query($koneksi,"SELECT sum(Jumlah) from master_pengeluaran where Id_divisi='11'");
-					echo number_format(mysqli_fetch_array($jumlah_kh)[000000]);
-					?>,
-					<?php 
-					$jumlah_logistik = mysqli_query($koneksi,"SELECT sum(Jumlah) from master_pengeluaran where Id_divisi='2'");
-					echo number_format(mysqli_fetch_array($jumlah_logistik)[000000]);
-					?>,
-					<?php 
-					$jumlah_pk = mysqli_query($koneksi,"SELECT sum(Jumlah) from master_pengeluaran where Id_divisi='10'");
-					echo number_format(mysqli_fetch_array($jumlah_pk)[000000]);
-					?>,
-					<?php 
-					$jumlah_pa = mysqli_query($koneksi,"SELECT sum(Jumlah) from master_pengeluaran where Id_divisi='5'");
-					echo number_format(mysqli_fetch_array($jumlah_pa)[000000]);
-					?>,
-					<?php 
-					$jumlah_PrPm = mysqli_query($koneksi,"SELECT sum(Jumlah) from master_pengeluaran where Id_divisi='7'");
-					echo number_format(mysqli_fetch_array($jumlah_PrPm)[000000]);
-					?>,
-					<?php 
-					$jumlah_Ri = mysqli_query($koneksi,"SELECT sum(Jumlah) from master_pengeluaran where Id_divisi='8'");
-					echo number_format(mysqli_fetch_array($jumlah_Ri)[000000]);
-					?>,
-					<?php 
-					$jumlah_SFHKI = mysqli_query($koneksi,"SELECT sum(Jumlah) from master_pengeluaran where Id_divisi='9'");
-					echo number_format(mysqli_fetch_array($jumlah_SFHKI)[000000]);
-					?>
->>>>>>> 92471ad0979c7f934ca2cb5898fa58b3d3d26ddd
 					],
 					backgroundColor: [
 					'rgba(255, 99, 132, 0.2)',
